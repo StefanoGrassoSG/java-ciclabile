c612d641eda93394e6f0d141caf676805f12ad93
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-17 16:16:25 +0100

    complete with bonus

--- src/org/lessons/java/javaciclabile/Main.java
+++ src/org/lessons/java/javaciclabile/Main.java
@@ -6,10 +6,26 @@ public class Main {
 		int[] numbers = {10, 20, 30, 40, 50};
 		
 		Numbers n = new Numbers(numbers);
+		n.addElemento(60);
 		
-		for(int x=0;x<numbers.length;x++) {
-			System.out.println(n.getElementoSuccessivo());
-			System.out.println(n.hasAncoraElementi());
-		}
+		Numbers f = new Numbers();
+		f.addElemento(1);
+		f.addElemento(2);
+		f.addElemento(3);
+		f.addElemento(4);
+		f.addElemento(5);
+		
+	   for (int i = 0; n.hasAncoraElementi(); i++) {
+            System.out.println(n.getElementoSuccessivo());
+            System.out.println(n.hasAncoraElementi());
+        }
+	   
+	   System.out.println("------------------------------");
+	   
+	   for (int i = 0; f.hasAncoraElementi(); i++) {
+           System.out.println(f.getElementoSuccessivo());
+           System.out.println(f.hasAncoraElementi());
+       }
+
 	}
 }
--- src/org/lessons/java/javaciclabile/Numbers.java
+++ src/org/lessons/java/javaciclabile/Numbers.java
@@ -8,6 +8,9 @@ public class Numbers {
 	public Numbers(int[] numbers) {
 		setNumbers(numbers);
 	}
+	public Numbers() {
+		this.numbers = new int[0];
+	}
 
 	public int[] getNumbers() {
 		return numbers;
@@ -19,12 +22,19 @@ public class Numbers {
 		this.currentIndex = 0;
 	}
 	
+	public void addElemento(int number) {
+		int[] nuovoArray = new int[numbers.length + 1];
+		for (int i = 0; i < numbers.length; i++) {
+            nuovoArray[i] = numbers[i];
+        }
+		nuovoArray[numbers.length] = number;
+		numbers = nuovoArray;
+	}
 	public int getElementoSuccessivo() {
 	   int nextI = numbers[currentIndex];
 	   currentIndex = currentIndex + 1;
         return nextI;
 	}
-	
 	public boolean hasAncoraElementi() {
 		if(currentIndex < numbers.length) {
 			return true;
